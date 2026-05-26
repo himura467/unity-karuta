181709c6dc880e5da74d617b838dd4686e552154
himura467 <akitoshitara@gmail.com>
2026-05-27 00:35:12 +0900

    feat: register NetworkRunner via GameLifetimeScope

---
+++ UniKaruta/Assets/_Project/Scripts/App/GameLifetimeScope.cs
@@ -0,0 +1,18 @@
+using Fusion;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace UniKaruta.Scripts.App
+{
+    public class GameLifetimeScope : LifetimeScope
+    {
+        [SerializeField]
+        private NetworkRunner _runnerPrefab;
+
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.RegisterComponentInNewPrefab(_runnerPrefab, Lifetime.Singleton);
+        }
+    }
+}
